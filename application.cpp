dd202b9d8af3bbf180437546db5ab2291e6396ff
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-04-05 10:26:06 +0300

    updated! to read from the file instead of terminal

--- application.cpp
+++ application.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 #include <string>
-
+#include <fstream>
 using namespace std;
 
 int main() {
+    string filename = "input.txt"; 
     string input_value;
 
-    cout << "Enter your input: " << endl;
-    getline(cin, input_value);
+//    cout << "Enter your input: " << endl;
+//    getline(cin, input_value);
 
-    cout << "The Result: " << input_value << endl;
+
+ ifstream input_file(filename);
+    if (input_file.is_open()) {
+
+        while (getline(input_file, input_value)) {
+            cout << input_value << endl;
+        }
+        input_file.close();
+    } else {
+        cout << "Error: Unable to open file" << endl;
+    }
+
+
+
+
+//    cout << "The Result: " << input_value << endl;
 
     return 0;
 }
+
+
+
