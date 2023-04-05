5bce286d38d2861c6e2aaf84e7ab85d978baaa7c
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-04-05 09:53:53 +0300

    I will be using C++ for this app.
    I will be using C++ for this application because I can code easily in this language and also the file management and using terminal for this application is just easy and C++ code can be run on any Operating system.

---
+++ application.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string input_value;
+
+    cout << "Enter your input: " << endl;
+    getline(cin, input_value);
+
+    cout << "The Result: " << input_value << endl;
+
+    return 0;
+}
